66efb661e5486842a16730a44f56cda93496268f
rubyspch <ruby_chohan@hotmail.co.uk>
2021-05-26 14:53:45 +0200

    Create index.js
    Create js file

---
+++ index.js
@@ -0,0 +1,5 @@
+/* I have the table but need to research more js/node stuff before i can do this part 
+function colourChange(){
+    "THE TD TAGS".innerHTML+="<td style='color:orange'></td>"
+};
+*/
