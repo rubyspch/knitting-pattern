90b68bd90a08bfd26c58bfa74d8ec5f05e81845b
rubyspch <ruby_chohan@hotmail.co.uk>
2021-05-26 15:31:56 +0200

    Update index.js
    Add variables and event listener to read users inputted colour

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+var currentColour = "";
+var button = document.getElementById("button");
+var input = document.getElementById("colourInput");
+button.addEventListener("click", function(){
+    if (input.value.length > 0){
+        currentColour = input.value;
+    }
+    button.style.backgroundColor = currentColour;
+})
+
 $(document).ready(function(){
     $("td").on({
         click: function(){ //when td is clicked make it orange
-            $(this).css("background-color","orange");
+            $(this).css("background-color",currentColour);
         },
         dblclick: function(){ //when td is double clicked go back to white
             $(this).css("background-color","white");
