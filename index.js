f36abeccd0f68acca02356af57e8695dc063d925
rubyspch <ruby_chohan@hotmail.co.uk>
2021-05-26 15:46:58 +0200

    Update index.js
    Add event listener for background colour change

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var currentColour = "";
+var currentColour = "white";
 var button = document.getElementById("button");
 var input = document.getElementById("colourInput");
-button.addEventListener("click", function(){
-    if (input.value.length > 0){
-        currentColour = input.value;
-    }
+
+button.addEventListener("click", function(){ // When button is clicked, change button background and currentColour to inputted value
+    currentColour = input.value;
     button.style.backgroundColor = currentColour;
+})
+
+var currentBackground = "white";
+var bgButton = document.getElementById("bgButton");
+var bgInput = document.getElementById("bgInput");
+var allTD = document.getElementsByTagName("td");
+
+bgButton.addEventListener("click", function(){ // When bgButton is clicked, change colour of button and all tds to inputted value
+    currentBackground = bgInput.value;
+    bgButton.style.backgroundColor = currentBackground;
+    for (let box of allTD){
+        box.style.backgroundColor = currentBackground;
+    }
 })
 
 $(document).ready(function(){
@@ -14,7 +26,7 @@ $(document).ready(function(){
             $(this).css("background-color",currentColour);
         },
         dblclick: function(){ //when td is double clicked go back to white
-            $(this).css("background-color","white");
+            $(this).css("background-color",currentBackground);
         }
     })
 });
